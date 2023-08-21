b9301aa8bacdb78cd3c504d191cc51f632da6a54
Ayoola Fakoya <ayoolafakoya@gmail.com>
2023-08-21 19:29:49 +0100

    new web

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // cssModules: false,
+}
+
+
+module.exports = nextConfig
+
+
+
+// const withSass = require('@zeit/next-sass');
+
+// const nextConfig = {
+//   /* Your other Next.js configuration options */
+// };
+
+// module.exports = withSass(nextConfig);
+
+// module.exports = {
+//     webpack: (config) => {
+//       config.module.rules.push({
+//         test: /\.(scss|sass)$/,
+//         use: ['sass-loader'],
+//       });
+  
+//       return config;
+//     },
+//   };
